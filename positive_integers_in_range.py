4a94b9e836ae45c1fd3cb67aa57cdcac607a2ac4
Samveg007 <84231614+Samveg007@users.noreply.github.com>
2022-07-20 14:41:03 +0530

    Add files via upload

---
+++ positive_integers_in_range.py
@@ -0,0 +1,12 @@
+list1 = [12,-7,5,64,-14]
+list2 = [12,14,-95,3]
+
+def positive_integer(list):
+    for n in list:
+        if n>0:
+         print(n)
+         
+
+         
+print(positive_integer(list1))
+print(positive_integer(list2))
